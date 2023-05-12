524a140d59dbb523f27024f59af94676bec8e959
TerenceNgai <130699154+TerenceNgai@users.noreply.github.com>
2023-05-12 14:45:26 -0400

    main.java

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
 
+class Luhn {
+  public static void main(String[] args) {
+    System.out.println("
+                       Customer and Sales system\n");
+    
+    
+  }
+}
